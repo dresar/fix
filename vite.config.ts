3d8306142320b14f83bd5737bd694479a625c549
EKA SYARIF MAULANA <114118210+dresar@users.noreply.github.com>
2026-02-23 21:46:51 +0700

    p

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
     host: true, // Listen on all addresses
     port: 8084,
@@ -75,14 +76,14 @@ export default defineConfig(({ mode }) => ({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
           },
         ],
       },
